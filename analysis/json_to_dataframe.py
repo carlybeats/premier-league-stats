4c2760783209efbdf09a9817102cbce62058e398
Carlos Byrne <carlosbyrne@outlook.com>
2025-03-31 19:50:25 +0100

    saved dataframe into csv format to then enable compatibility with data visualisation tools

--- analysis/json_to_dataframe.py
+++ analysis/json_to_dataframe.py
@@ -1,8 +1,9 @@
 import pandas as pd
 import os
 
+root_dir = os.path.dirname(os.path.dirname(__file__))
+
 def retrieve_filepath():
-    root_dir = os.path.dirname(os.path.dirname(__file__))
     #print(root_dir)
     if os.environ.get("TESTING") != 'True':
         filepath = os.path.join(root_dir, "data/goals.json")
@@ -19,3 +20,11 @@ def base_dataframe():
     df.loc[df['name'] == 'Beto', 'team'] = 'Everton'
     df.dropna(inplace=True)
     return df
+
+def store_to_csv():
+    df = base_dataframe()
+    filepath = os.path.join(root_dir, "data/goals.csv")
+    df.to_csv(filepath, index=False)  
+
+if __name__ == '__main__':
+    store_to_csv()
